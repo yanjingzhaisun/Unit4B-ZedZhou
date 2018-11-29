0e0619d4d3304d8d5186d0397fa137754556b3df
Zijian Zhou <yanjingzhaisun@gmail.com>
2018-11-29 16:31:40 -0500

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    float timer;
+    public float healthPercentage;
+    public float infectionSpeed;
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0;
+        healthPercentage = 1f; //100%
+        infectionSpeed = 0.005f; //0.5%
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 1f)
+        {
+            Debug.Log("1 second!");
+            timer = 0;
+            healthPercentage -= infectionSpeed;
+        }
+    }
+}
